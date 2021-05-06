2eece56694fd9f2b1a90c2fb36f003ba611e9811
Millennial-Polymath <muchamingotho@gmail.com>
2021-05-06 09:58:37 +0000

    Adda a function to find the biggest integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        y = 0
+        for x in my_list:
+            if x > y:
+                y = x
+        return y
+    else:
+        return None
