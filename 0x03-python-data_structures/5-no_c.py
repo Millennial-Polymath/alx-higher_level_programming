4e24a847539d30745912fd2be4786fb1cc9ad39a
Millennial-Polymath <muchamingotho@gmail.com>
2021-05-06 07:42:01 +0000

    Made the code compact

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = my_string.translate({ord('c'): None})
-    new_str = my_string.translate({ord('C'): None})
+    new_str = my_string.translate({ord(i): None for i in 'cC'})
     return new_str
