969144df9914ff73dca56e7592c732aaedb6f41e
Millennial-Polymath <muchamingotho@gmail.com>
2021-05-07 18:48:09 +0000

    Print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    dict_items = a_dictionary.items() # call items to return key-value pairs
-    sorted_dict = dict(sorted(dict_items, key=lambda kv: kv[0].upper())) # returns tuples
-    for key, value in sorted_dict.items():
-        print(key, ':', value)
+    sorted_list = sorted(a_dictionary.keys())
+    for key in sorted_list:
+        print(key, ':', a_dictionary[key])
