31523fdf3d493f3dd44ffb89130f172055810efb
Millennial-Polymath <muchamingotho@gmail.com>
2021-06-11 22:43:10 +0000

    Further debug

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-contains class Square
+Module Square
+Inherits from class Rectangle,
+That inherits from class Base
 """
 
 
@@ -8,8 +10,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    class Square:
+    """class Square inherits from class Rectangle
     Methods:
         def __init__(self,size,x,y,id)
         def __str__(self)
@@ -21,57 +22,60 @@ class Square(Rectangle):
         def size(self, value)
     """
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialization
+        calls the supper class rectangle
+        and assigns width and height to size
         """
-        Initialiases the attributes
-        """
-       super().__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
+        self.size = size
 
     @property
     def size(self):
-        """
-        public getter for attribute size
-        """
+        """Size getter"""
         return self.width
+
     @size.setter
     def size(self, value):
-        """
-        public setter for attribute size
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """size setter"""
         self.width = value
         self.height = value
 
-    def update(self, *args, **kwargs):
-        """
-        Assign args to attributes
-        """
-        if len(args) ==  0:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-        try:
-            self.id = args[0]
-            self.size = args[1]
-            self.x = args[2]
-            self.y = args[3]
-        except IndexError:
-            pass
-
     def __str__(self):
+        """Overrides to return
+        "[Square] (<id>) <x>/<y> - <size>"
         """
-        returns string version of the attributes
-        """
-        return ("[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
-                                              self.id, self.x,
-                                              self.y, self.width))
+        return ("[Square] ({}) {}/{} - {}".format
+                (self.id, self.x, self.y, self.size))
 
-    def get_to_dictionary(self):
-        """
-        Returns the dictionary representation of a square
-        """
-        return {'id': getattr(self, 'id'),
-                'size': getattr(self, 'size'),
-                'x': getattr(self, 'x'),
-                'y': getattr(self, 'y')}
+    def update(self, *args, **kwargs):
+        """Assign arguments to attributes"""
+        if args:
+            for i, j in enumerate(args):
+                if i == 0:
+                    self.id = j
+                elif i == 1:
+                    self.size = j
+                elif i == 2:
+                    self.x = j
+                else:
+                    self.y = j
+
+        else:
+            """kwargs"""
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """returns the dictionary representation of square"""
+        dic = {}
+        dic["id"] = self.id
+        dic["size"] = self.size
+        dic["x"] = self.x
+        dic["y"] = self.y
+        return dic
