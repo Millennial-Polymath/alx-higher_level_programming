5c0c91dcde63ebd654684228f69ed3440e0e2ad4
Millennial-Polymath <muchamingotho@gmail.com>
2021-06-11 22:38:35 +0000

    Fix documentation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,18 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    class Square:
+    Methods:
+        def __init__(self,size,x,y,id)
+        def __str__(self)
+        def update(self, *args, **kwargs)
+        def to_dictionary(self)
+    Getter:
+       def size(self)
+    Setter:
+        def size(self, value)
+    """
     def __init__(self, size, x=0, y=0, id=None):
         """
         Initialiases the attributes
