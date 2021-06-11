8360728207ad48377da96435c993b15fd3bb58aa
Millennial-Polymath <muchamingotho@gmail.com>
2021-06-11 22:19:38 +0000

    Fixed documentation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,10 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
+        """
+        Initialiases the attributes
+        """
+       super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
